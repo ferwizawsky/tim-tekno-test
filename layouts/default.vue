0ae8c430841fb0273be4c1dca37608834122f4da
ferwizawsky <ferryspedia14@gmail.com>
2024-03-15 10:45:31 +0700

    add(pages): stopwatch

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
+import { RouterLink } from "vue-router";
 
 const list = [];
 const colorMode = useColorMode();
@@ -17,7 +18,7 @@ function darkChange(event: string) {
       <OverstoreLogoIcon class="cursor-pointer" />
       <div>
         <div class="flex items-center space-x-2">
-          <div class="space-x-4 pr-10">
+          <div class="space-x-4 pr-10 hidden lg:block">
             <a href="#" class="text-primary font-semibold">Home</a>
             <a href="#">Whitelist</a>
             <a href="#">About</a>
@@ -31,7 +32,14 @@ function darkChange(event: string) {
         </div>
       </div>
     </div>
-    <div class="p-4">
+
+    <div
+      class="p-4 flex items-center fixed bottom-0 w-full z-20 bg-background/30 backdrop-blur-sm"
+    >
+      <RouterLink to="/">Soal 1</RouterLink>
+      <RouterLink to="/stopwatch"> Soal 2</RouterLink>
+    </div>
+    <div class="p-4 pb-10">
       <slot />
     </div>
   </div>
